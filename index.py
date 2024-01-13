c54c64f8fce6a814e9646ac100a28861ac935c8d
malpkakefirek <malpkakefirek@gmail.com>
2024-01-13 22:11:48 +0100

    typo

--- index.py
+++ index.py
@@ -654,7 +654,7 @@ def search_users():
 
         sql = "SELECT uuid, username, avatar FROM users WHERE username ~* %s AND uuid !~* %s"
         values = (query, user_id)
-        cursor.execute(sql, value)
+        cursor.execute(sql, values)
         friends = cursor.fetchall()
         cursor.close()
 
